81f9a709cbb3b36649a0bec34612b254e88aebf3
Luca Guzzon <luca.guzzon@gmail.com>
2026-02-25 11:03:26 +0000

    test(cypher): add test file and runner module

--- js/Cypher.run.cjs
+++ js/Cypher.run.cjs
@@ -4,22 +4,22 @@ var Cypher = require("./Cypher.js");
 var cypherTest = new Cypher();
 
 var statements = [
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-nodes.csv" as l \
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-nodes.csv" as l \
     merge (c:Character{name:l.Id}) \
     return count(1)',
 
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
     match (source:Character{name:l.Source}), (target:Character{name:l.Target}) \
     merge (source)-[r:KNOWS{weight:l.Weight}]->(target) \
     return count(1)',
 
-    'match (a:Character)-[:KNOWS]->(b:Character) \
-    return count(1) as cnt',
+  "match (a:Character)-[:KNOWS]->(b:Character) \
+    return count(1) as cnt",
 
-    'match (a:Character)-[:KNOWS*]->(b:Character) \
-    return count(1) as cnt',
-    
-    'merge (:Node{ID:1})-[:TO{ID:12}]->(:Node{ID:2}) \
+  "match (a:Character)-[:KNOWS*]->(b:Character) \
+    return count(1) as cnt",
+
+  "merge (:Node{ID:1})-[:TO{ID:12}]->(:Node{ID:2}) \
     merge (:Node{ID:2})-[:TO{ID:23}]->(:Node{ID:3}) \
     merge (:Node{ID:2})-[:TO{ID:24}]->(:Node{ID:4}) \
     merge (:Node{ID:3})-[:TO{ID:35}]->(:Node{ID:5}) \
@@ -28,51 +28,51 @@ var statements = [
     merge (:Node{ID:4})-[:TO{ID:47}]->(:Node{ID:7}) \
     with 1 as dummy \
     match (a)-[r:TO]->(b) \
-    return count(1)',
+    return count(1)",
 
-    'match (a:Node{ID:1})-[r*]->(b) \
-    return size(r)',
-    
-    'merge (:Node{ID:1})-[:TO]->(:Node{ID:2}) merge (:Node{ID:1})-[:TO]->(:Node{ID:3}) return count(1)',
+  "match (a:Node{ID:1})-[r*]->(b) \
+    return size(r)",
 
-    'merge (:Node{ID:1})-[:TO]->(:Node{ID:2}) merge (:Node{ID:1})-[:TO]->(:Node{ID:3}) return count(1)',
+  "merge (:Node{ID:1})-[:TO]->(:Node{ID:2}) merge (:Node{ID:1})-[:TO]->(:Node{ID:3}) return count(1)",
 
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-nodes.csv" as l \
+  "merge (:Node{ID:1})-[:TO]->(:Node{ID:2}) merge (:Node{ID:1})-[:TO]->(:Node{ID:3}) return count(1)",
+
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-nodes.csv" as l \
     merge (c:Character{name:l.Id}) \
     return count(1)',
 
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
     match (source:Character{name:l.Source}), (target:Character{name:l.Target}) \
     merge (source)-[r:KNOWS{weight:l.Weight}]->(target) \
     return count(1)',
-    
-    'match (a:Character{name:"Edmure"})-[:KNOWS*]->(:Character) \
+
+  'match (a:Character{name:"Edmure"})-[:KNOWS*]->(:Character) \
     return a.name, count(1)',
 
-    'LOAD CSV WITH HEADERS FROM "https://niclasko.github.io/data/premier_league_2017_2018.csv" AS csvLine \
+  'LOAD CSV WITH HEADERS FROM "https://niclasko.github.io/data/premier_league_2017_2018.csv" AS csvLine \
     merge (ht:SoccerTeam{name:csvLine.HomeTeam}) merge (at:SoccerTeam{name:csvLine.AwayTeam}) \
     with csvLine, ht, at \
     merge (ht)-[r:PLAYED{result:csvLine.FTR, home_goals: toInt(csvLine.FTHG), away_goals: toInt(csvLine.FTAG)}]->(at)',
-    'match (:SoccerTeam)-[:PLAYED*..3]->(:SoccerTeam) \
-    return count(1)',
+  "match (:SoccerTeam)-[:PLAYED*..3]->(:SoccerTeam) \
+    return count(1)",
 
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
     merge (:Character{name:l.Source}) merge (:Character{name:l.Target}) \
     with l \
     match (source:Character{name:l.Source}), (target:Character{name:l.Target}) \
     merge (source)-[r:KNOWS{weight:l.Weight}]->(target) \
     return count(1)',
-    
-    'unwind [{k:"a", v: 1}, {k:"b", v: 2}] as e return collect(distinct e)',
-    
-    'unwind ["a","a","b","b","c","d","a"] as e with barchart(e) as bc, {kiwi: 2} as k return bc.a, k.kiwi',
 
-    'unwind range(1,10001) as e return barchart(round(rand()*10))',
-    
-    'unwind range(1,100001) as e \
-    return histogram(round(rand()*1000), 6)',
+  'unwind [{k:"a", v: 1}, {k:"b", v: 2}] as e return collect(distinct e)',
 
-    'merge (computer:Item{id:"Computer"}) \
+  'unwind ["a","a","b","b","c","d","a"] as e with barchart(e) as bc, {kiwi: 2} as k return bc.a, k.kiwi',
+
+  "unwind range(1,10001) as e return barchart(round(rand()*10))",
+
+  "unwind range(1,100001) as e \
+    return histogram(round(rand()*1000), 6)",
+
+  'merge (computer:Item{id:"Computer"}) \
     merge (hdd:Item{id:"HDD"}) \
     merge (keyboard:Item{id:"Keyboard"}) \
     merge (motherboard:Item{id:"Motherboard"}) \
@@ -90,25 +90,25 @@ var statements = [
     where not((pid)-[:PARENT]->(:Item)) \
     unwind nodes(p) as item \
     return nodes(p)[0].id, collect(item.id) as item_path',
-    
-    'create (:A)-[:CONNECT]->(:B), \
+
+  "create (:A)-[:CONNECT]->(:B), \
             (:A)-[:CONNECT]->(:B) \
     match ()-[r]->() \
     with collect(r) as rels \
     unwind rels as rel \
-    return startnode(rel), rel, endnode(rel)',
-    
-    'return case when 1=1 then 1 else 0 end', 
+    return startnode(rel), rel, endnode(rel)",
 
-    'with range(1,7) as faces \
+  "return case when 1=1 then 1 else 0 end",
+
+  "with range(1,7) as faces \
     unwind faces as d1 \
     unwind faces as d2 \
     unwind faces as d3 \
     unwind faces as d4 \
     unwind faces as d5 \
-    return d1, d2, d3, d4, d5 where d1 = d2 or d2 = d3',
+    return d1, d2, d3, d4, d5 where d1 = d2 or d2 = d3",
 
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
     merge (:Character{name:l.Source}) merge (:Character{name:l.Target}) \
     with l \
     match (source:Character{name:l.Source}), (target:Character{name:l.Target}) \
@@ -117,23 +117,23 @@ var statements = [
     match (:Character)-[:KNOWS*]->(:Character) \
     return count(1)',
 
-    'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
+  'load csv with headers from "https://raw.githubusercontent.com/melaniewalsh/sample-social-network-datasets/master/sample-datasets/game-of-thrones/got-edges.csv" as l \
     merge (source:Character{name:l.Source}) merge (target:Character{name:l.Target}) \
     merge (source)-[r:KNOWS{weight:l.Weight}]->(target) with 1 as dummy \
     match p=(:Character{name:"Salladhor"})-[*..5]-(:Character{name:"Petyr"}) \
     return count(1)',
 
-    'create (n1:Node{name:"n1"})-[:CONNECTED]->(n2:Node{name:"n2"})-[:CONNECTED]->(n3:Node{name:"n3"}), \
+  'create (n1:Node{name:"n1"})-[:CONNECTED]->(n2:Node{name:"n2"})-[:CONNECTED]->(n3:Node{name:"n3"}), \
     (n3)-[:CONNECTED]->(n4:Node{name:"n4"}) \
     create (n2)-[:CONNECTED]->(n5:Node{name:"n5"})-[:CONNECTED]->(n3) \
     match p=(:Node)-[:CONNECTED*]->(:Node) \
     return count(1)',
 
-    'merge (a:Node{id:0}) merge (b:Node{id:1}) merge (a)-[r:TO]->(b) return r',
+  "merge (a:Node{id:0}) merge (b:Node{id:1}) merge (a)-[r:TO]->(b) return r",
 
-    'return tojson(\'{"a":1}\').a',
+  "return tojson('{\"a\":1}').a",
 
-    'create (a:Node{id:0}) \
+  "create (a:Node{id:0}) \
     create (b:Node{id:1}) \
     create (c:Node{id:2}) \
     create (d:Node{id:3}) \
@@ -145,9 +145,9 @@ var statements = [
     match p=(:Node)-[:PARENT*]->(parent:Node) \
     where not((parent)-[:PARENT]->(:Node)) \
     unwind nodes(p) as node \
-    return id(nodes(p)[0]) as leaf, collect(id(node)) as path',
+    return id(nodes(p)[0]) as leaf, collect(id(node)) as path",
 
-    "CREATE (yoda:Contact {name: 'Yoda', email: 'yoda@lucasfilm.com'}) \
+  "CREATE (yoda:Contact {name: 'Yoda', email: 'yoda@lucasfilm.com'}) \
     CREATE (lukeskywalker:Contact {name: 'Luke Skywalker', email: 'luke.skywalker@lucasfilm.com'}) \
     CREATE (chewbacca:Contact {name: 'Chewbacca', email: 'chewbacca@lucasfilm.com'}) \
     CREATE (r2d2:Contact {name: 'R2D2', email: 'r2d2@lucasfilm.com'}) \
@@ -215,7 +215,7 @@ var statements = [
     unwind nodes(p) as member \
     return nodes(p)[0].name as leaf, collect(member.name) as path",
 
-    "unwind range(1,100) as i \
+  "unwind range(1,100) as i \
     create (n:Node{id:i}) \
     with collect(n) as nodes \
     where size(nodes) > 1 \
@@ -228,20 +228,20 @@ var statements = [
     match (_n1)-[:TO]->(_n2) \
     return count(1)",
 
-    "match (n1)-[r:TO]->(n2) \
+  "match (n1)-[r:TO]->(n2) \
     return count(1)",
 
-    "with [[3,2,5,1,6]] as sets1, [[1,2,3,4,5]] as sets2 \
+  "with [[3,2,5,1,6]] as sets1, [[1,2,3,4,5]] as sets2 \
     unwind sets1 as set1 \
     with set1, size(set1) as size1 \
     return set1",
 
-    "with range(0,9) as set1 \
+  "with range(0,9) as set1 \
     unwind set1 as item \
     with size(set1) as x \
     return *",
 
-    "unwind range(0,5) as i \
+  "unwind range(0,5) as i \
     merge (n:Node{_id:i}) \
     with collect(n) as _nodes \
     unwind _nodes as n1 \
@@ -249,35 +249,35 @@ var statements = [
     merge (n1)-[:rel]->(n2) \
     return count(1)",
 
-    "unwind [2,3] as len \
+  "unwind [2,3] as len \
     unwind range(0, len) as i \
     with len, collect(i) as is \
     where size(is) < 3 \
-    return is, size(is)"
+    return is, size(is)",
 ];
 
 async function run() {
-    for(i=0; i<statements.length; i++) {
-        await new Promise(async (resolve, reject) => {
-            var statement = statements[i];
-            
-            await cypherTest.execute(
-                statement,
-                function(results) {
-                    console.log();
-                    console.log(statement);
-                    console.log(JSON.stringify(results.graph));
-                    console.log(JSON.stringify(results));
-                    resolve();
-                },
-                function(error) {
-                    console.log(error);
-                    resolve();
-                }
-            );
-        });
-    }
-    console.log();
+  for (i = 0; i < statements.length; i++) {
+    await new Promise(async (resolve, reject) => {
+      var statement = statements[i];
+
+      await cypherTest.execute(
+        statement,
+        function (results) {
+          console.log();
+          console.log(statement);
+          console.log(JSON.stringify(results.graph));
+          console.log(JSON.stringify(results));
+          resolve();
+        },
+        function (error) {
+          console.log(error);
+          resolve();
+        },
+      );
+    });
+  }
+  console.log();
 }
 
 run();
